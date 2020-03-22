c162109fecff5fa016a092dd31fd15c197468c4b
maxLozhechnikov <61139039+maxLozhechnikov@users.noreply.github.com>
2020-03-22 11:26:33 +0300

    1

---
+++ lab_2/print_args/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace print_args
+{
+    class Program
+    {
+        static void Main(string[] arguments)
+        {
+            if (arguments.Length == 0)
+            {
+                Console.WriteLine("No parameters were specified!");
+            }
+            else
+            {
+                Console.WriteLine("Number of arguments: " + arguments.Length);
+                Console.Write("Arguments: ");
+                foreach (string argument in arguments)
+                {
+                    Console.Write(argument + " ");
+                }
+            }
+        }
+    }
+}
---
+++ lab_2/remove_duplicates/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace remove_duplicates
+{
+    class Program
+    {
+        static void Main(string[] arguments)
+        {
+            if (arguments.Length != 1)
+            {
+                Console.WriteLine("Incorrect number of arguments!");
+                Console.WriteLine("Usage RemoveDuplicates.exe <input string>");
+            }
+            else
+            {
+                HashSet<char> symbols_set = new HashSet<char>();
+                string input = arguments[0];
+                char temp;
+                for (int i = 0; i < input.Length; i++)
+                {
+                    temp = input[i];
+                    if (symbols_set.Contains(temp))
+                    {
+                        input = input.Remove(i, 1);
+                        i--;
+                    }
+                    else
+                    {
+                        symbols_set.Add(temp);
+                    }
+                }
+
+                Console.WriteLine(input);
+            }
+        }
+    }
+}
