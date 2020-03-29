addb77105cc9ab597a35e9a7b7d55e61af05b91c
maxLozhechnikov <61139039+maxLozhechnikov@users.noreply.github.com>
2020-03-29 14:41:38 +0300

    added 3.1

---
+++ lab_3/check_identifier/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace check_identifier
+{
+    public class Program
+    {
+        static bool is_letter(char ch)
+        {
+            return (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z');
+        }
+        static bool is_digit(char ch)
+        {
+            return (ch >= '0' && ch <= '9');
+        }
+
+        public static int Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Wrong parameters count");
+                return 1;
+            }
+            else
+            {
+                string input = args[0];
+                if (input.Length > 0)
+                {
+                    if (is_digit(input[0]))
+                    {
+                        Console.WriteLine("no");
+                        Console.WriteLine("identifier must be started with letter");
+                        return 1;
+                    }
+                    for (int i = 1; i < input.Length; i++)
+                    {
+                        if (!is_letter(input[i]) && !is_digit(input[i]))
+                        {
+                            Console.WriteLine("no");
+                            Console.WriteLine("identifier must contain only letters or numbers");
+                            return 1;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("no");
+                    Console.WriteLine("Empty string is not identifier");
+                    return 1;
+                }
+            }
+            Console.WriteLine("yes");
+            return 0;
+        }
+    }
+}
---
+++ lab_3/check_identifier_tests/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using check_identifier;
+
+namespace check_identifier_tests
+{
+    [TestClass]
+    public class check_identifier_tests
+    {
+        [TestMethod]
+        public void numberInBegging_numberInStartOfInput_renutrErr()
+        {
+            string[] wrong1 = new string[1] { "4qwerty" };
+            int min;
+            min = Program.Main(wrong1);
+            Assert.AreEqual(1, min);
+        }
+        [TestMethod]
+        public void incorrectSymbol_unexpectedSymbolInInput_returnErr()
+        {
+            string[] wrong1 = new string[1] { "werty^tt" };
+            int min;
+            min = Program.Main(wrong1);
+            Assert.AreEqual(1, min);
+        }
+        [TestMethod]
+        public void normalWork_correctInput_return0()
+        {
+            string[] wrong1 = new string[1] { "qwerty567" };
+            int min;
+            min = Program.Main(wrong1);
+            Assert.AreEqual(0, min);
+        }
+        [TestMethod]
+        public void incorrectCountArguments_moreThenOneArgument_returnErr()
+        {
+            string[] wrong1 = new string[2] { "qwertyu678", "7ghgnjh76" };
+            int min;
+            min = Program.Main(wrong1);
+            Assert.AreEqual(1, min);
+        }
+        [TestMethod]
+        public void incorrectCountArguments_lessThenOneArgument_returnErr()
+        {
+            string[] wrong1 = new string[0] { };
+            int min;
+            min = Program.Main(wrong1);
+            Assert.AreEqual(1, min);
+        }
+        [TestMethod]
+        public void emptyString_emptyInput_returnErr()
+        {
+            string[] wrong1 = new string[1] { "" };
+            int min;
+            min = Program.Main(wrong1);
+            Assert.AreEqual(1, min);
+        }
+    }
+}
