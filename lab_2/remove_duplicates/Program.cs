f584137619dbcfd715a119812881af4148bc5c9c
maxLozhechnikov <61139039+maxLozhechnikov@users.noreply.github.com>
2020-06-22 20:45:04 +0300

    Updated RemoveDuplicates

--- lab_2/remove_duplicates/Program.cs
+++ lab_2/remove_duplicates/Program.cs
@@ -12,27 +12,31 @@ namespace remove_duplicates
                 Console.WriteLine("Incorrect number of arguments!");
                 Console.WriteLine("Usage RemoveDuplicates.exe <input string>");
             }
-            else
-            {
-                HashSet<char> symbols_set = new HashSet<char>();
+            else {
                 string input = arguments[0];
-                char temp;
-                for (int i = 0; i < input.Length; i++)
-                {
-                    temp = input[i];
-                    if (symbols_set.Contains(temp))
-                    {
-                        input = input.Remove(i, 1);
-                        i--;
-                    }
-                    else
-                    {
-                        symbols_set.Add(temp);
-                    }
-                }
-
+                input = WorkWithString(input);
                 Console.WriteLine(input);
             }
+        }
+
+        static string WorkWithString(string input)
+        {
+            HashSet<char> symbols_set = new HashSet<char>();
+            char temp;
+            for (int i = 0; i < input.Length; i++)
+            {
+                temp = input[i];
+                if (symbols_set.Contains(temp))
+                {
+                    input = input.Remove(i, 1);
+                    i--;
+                }
+                else
+                {
+                    symbols_set.Add(temp);
+                }
+            }
+            return input;
         }
     }
 }
